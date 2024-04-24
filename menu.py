5e44157838247904483df6635b61c4189a6299a5
Serhat Baykal <mukhtafi@pm.me>
2024-04-24 09:41:31 +0200

    use os.pathsep to work on windows and linux

--- menu.py
+++ menu.py
@@ -5,8 +5,8 @@ W_hotboxCommonPath = os.getenv('W_HOTBOXCOMON_REPO', "")
 nuke.pluginAddPath(W_hotboxCommonPath)
 
 os.environ['W_HOTBOX_ICONS'] = f"{W_hotboxCommonPath}default_buttons/icons"
-os.environ['W_HOTBOX_REPO_PATHS'] = f"{W_hotboxCommonPath}default_buttons:{W_hotboxCommonPath}mgl_buttons"
-os.environ['W_HOTBOX_REPO_NAMES'] = "default_buttons:mgl_buttons"
+os.environ['W_HOTBOX_REPO_PATHS'] = f"{W_hotboxCommonPath}default_buttons{os.pathsep}{W_hotboxCommonPath}mgl_buttons"
+os.environ['W_HOTBOX_REPO_NAMES'] = f"default_buttons{os.pathsep}mgl_buttons"
 os.environ['W_HOTBOX_HIDE_ICON_LOC'] = 'True'
 os.environ['W_HOTBOX_MGLICONS'] = f"{W_hotboxCommonPath}mgl_buttons/icons"
 
